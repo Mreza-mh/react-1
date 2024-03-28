6024e3bacc639459159a10fd0d51999a0a816669
M.Reza <mohrezmehri14@gmail.com>
2024-03-28 17:44:00 +0330

    Edit some minor things

--- src/maincomponents/Mainbody.jsx
+++ src/maincomponents/Mainbody.jsx
@@ -12,15 +12,15 @@ function Mainbody({ pitems }) {
 
     return (
         <>
-            <div className='container mx-auto px-4  '>
+            <div className='container mx-auto px-3  '>
             <h2 className="text-4xl font-bold text-center text-emerald-50 mb-8 py-10 font-serif  border-b-4 border-[#9b90a8] mt-8 ">	-	Last 3 posts -	</h2>
-            <p className='text-[#d1a74b71] ml-5  '> hover on images ... </p>
+            <p className='text-[#d1a74b71] ml-5 mb-5  '> hover on images ... </p>
             <div className=' '>
             <div className='flex flex-wrap justify-around '>
             
                         {lastThreeItems.map((item, i) =>(
 
-                        <div className=' flex flex-col w-full m-20  xl:w-1/5 lg:w-1/4 md:w-1/3  sm:w-1/2 sm:m-10 ' key={i}>
+                        <div className=' flex flex-col w-full mx-20 my-8 xl:w-1/5 lg:w-1/4 md:w-1/3  sm:w-1/2 sm:m-10 ' key={i}>
                             <div>
                                 <div className={`border-none shadow-inner shadow-[#372641a1] transition-transform transform origin-bottom 
                                 p-3 bg-[#42364981] rounded-t-lg text-white text-center text-xl font-semibold
